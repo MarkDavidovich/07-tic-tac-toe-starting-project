5e0d6cd5157252d7ca38c1d46989f6568da7966b
Mark <mark.davidovich@gmail.com>
2025-06-17 04:50:29 +0300

    add edit name functionality add game board component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,38 @@
-export default function Player({name, symbol}) {
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing); // By using a function we guarantee that we are working with the latest value of the state (instead of scheduling).
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let btnCaption = "Edit";
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        className="player-name"
+        onChange={handleChange}
+      ></input>
+    );
+    btnCaption = "Save";
+  }
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
-        <button>Edit</button>
+        <button onClick={handleEditClick}>{btnCaption}</button>
       </span>
     </li>
   );
